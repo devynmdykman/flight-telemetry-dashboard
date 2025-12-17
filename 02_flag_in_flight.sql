c93c0080742fe9a6c8d29f1dfa1f1f3ddc71542d
devynmdykman <devynmdykman@gmail.com>
2025-12-17 14:22:45 -0800

    Update 02_flag_in_flight.sql
    updated script

--- 02_flag_in_flight.sql
+++ 02_flag_in_flight.sql
@@ -1,1 +1,18 @@
+-- 02_flag_in_flight.sql
+-- Purpose:
+-- Define a conservative, robust "in_flight" flag to separate airborne segments from noise.
+-- I intentionally use BOTH altitude and airspeed (redundancy) because either sensor can be missing/noisy.
+-- Rule: in_flight if altitude > 100m OR airspeed > 30m/s.
+-- These thresholds are conservative and meant to capture clearly-airborne flight (not taxi).
+
+CREATE OR REPLACE VIEW telemetry_flagged AS
+SELECT
+  *,
+  CASE
+    WHEN altitude > 100
+      OR airspeed > 30
+    THEN TRUE
+    ELSE FALSE
+  END AS in_flight
+FROM telemetry_wide;
 
